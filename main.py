66bf404a0dc407edf9988177d99940ac575314ad
ankurkul86 <60247751+ankurkul86@users.noreply.github.com>
2024-11-05 01:32:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from chainlit.utils import mount_chainlit
+
 app = FastAPI()
 
 
@@ -9,6 +11,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
+# Mount the Chainlit application
+mount_chainlit(app=app, target="my_cl_app.py", path="/chainlit")
